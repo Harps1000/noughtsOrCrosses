82dea65cd98772af31efe61fb6b990b641abeeb7
Harps1000 <Harpreet101gill@gmail.com>
2019-07-25 15:42:31 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    board: ["nought", "nought", "cross", null, null, null, null, null, null,]
+  };
+
+  render() {
+    return (
+     <div className="container">
+       {this.state.board.map((square, i)=>{
+         return <div className={`square${i} ${square}`} onClick={ square && this.updateSquare(i)} ></div> 
+       })}
+     </div>
+    );
+  }
+
+  updateSquare = (i) =>{
+    this.setState((currentState) => {
+      currentState.board[i] = 'cross';
+      console.log(currentState);
+      return currentState;
+    })
+  }
+
+ 
 }
-
 export default App;
