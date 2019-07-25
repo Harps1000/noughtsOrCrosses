bde2e458b7c0d0b6d364f6dc1cad1c4c6e1490c2
James <j.clegg1@hotmail.co.uk>
2019-07-25 16:08:00 +0100

    working N and C need to check winner and reset

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./index.css";
 
 class App extends React.Component {
   state = {
-    board: ["nought", "nought", "cross", null, null, null, null, null, null,]
+    whosTurn: 'nought',
+    board: [null, null, null, null, null, null, null, null, null,]
   };
 
   render() {
     return (
-     <div className="container">
-       {this.state.board.map((square, i)=>{
-         return <div className={`square${i} ${square}`} onClick={ square && this.updateSquare(i)} ></div> 
-       })}
-     </div>
+      <div className="container">
+        {this.state.board.map((square, i) => {
+          return <div key={i} className={`square${i} ${square}`} onClick={() => { if (!square) this.updateSquare(i) }} ></div>
+        })}
+      </div>
     );
   }
 
-  updateSquare = (i) =>{
+  updateSquare = (i) => {
     this.setState((currentState) => {
-      currentState.board[i] = 'cross';
-      console.log(currentState);
-      return currentState;
+      if (this.state.whosTurn === 'nought') {
+        currentState.board[i] = 'nought';
+        currentState.whosTurn = 'cross';
+        console.log(currentState)
+        return currentState;
+      }
+      else {
+        currentState.board[i] = 'cross';
+        currentState.whosTurn = 'nought';
+        console.log(currentState)
+        return currentState;
+      }
     })
   }
 
- 
 }
 export default App;
